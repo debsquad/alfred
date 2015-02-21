f8d918ee3ad41c8c12b16d2742e1dc272bb69d34
vnn <vnn@yasin.io>
2015-02-21 03:20:30 +0100

    initial commit. add feature "pacontent"

---
+++ alfred.py
@@ -0,0 +1,57 @@
+import pyirclib
+import string
+import sys
+import time
+import re
+import os.path
+from random import randint
+
+# configuration
+quotesdb = 'quotes.db'
+server = 'irc.oftc.net'
+port = 6667
+chan = '#debsquad'
+
+# connexion
+irc = pyirclib.Irclib(server,port)
+irc.setDebug = 1
+irc.login('alfred',username = 'alfred')
+irc.join(chan)
+
+# parser
+def parsemessage(msg):
+    if msg['event'] == "PRIVMSG": # if msg in channel
+        message = str(msg['text']).replace('\r','') # we store/format it
+
+        # FEATURE: Pacontent
+        if re.search("^!Pacontent", message, re.IGNORECASE):
+            # check for database
+            if (not os.path.isfile(quotesdb)):
+                rc.privmsg(chan,"Unable to access database.")
+            # save entry
+            elif re.search("^!Pacontent \w+", message, re.IGNORECASE):
+                rmcmd = re.compile("!pacontent ", re.IGNORECASE)
+                message = rmcmd.sub('', message)
+                f = open(quotesdb,'a')
+                f.write(message + "\n")
+                f.close()
+            # show random entry
+            elif re.search("^!Pacontent$", message, re.IGNORECASE):
+                quotes = 0
+                for line in open(quotesdb):
+                    quotes += 1
+                quote = randint(1, quotes) # random number
+                count = 1 # count init
+                f = open(quotesdb,'r')
+                for line in f:
+                    if (count == quote):
+                        formatline = str(line)
+                        irc.privmsg(chan,formatline)
+                    count += 1
+
+# enable real time parsing
+while 1:
+  message = irc.getmessage()
+  print message
+  parsemessage(message)
+
