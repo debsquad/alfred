b3c45bd8621f0ae2d8c5ec7aecaa53e55a0738d8
vnn <vnn@yasin.io>
2015-02-24 03:29:30 +0100

    simplify modpacontent

--- mods/modpacontent.py
+++ mods/modpacontent.py
@@ -7,19 +7,18 @@ quotesdb = 'db/pacontent.db'
 def process(e):
     a = e.arguments[0].split(' ', 1)
 
+    # save
     if len(a) > 1:
         msg = a[1].encode('utf-8')
-
         with open(quotesdb, 'a') as fp:
             fp.write(msg + "\n")
-
         return "Quote saved."
-    else:
-        with open(quotesdb) as fp:
-            quotes = fp.readlines()
 
-            if quotes:
-                quote = random.choice(quotes).strip()
-                return quote.decode('utf-8')
-            else:
-                return "Database is empty"
+    # show
+    with open(quotesdb) as fp:
+        quotes = fp.readlines()
+        if quotes:
+            quote = random.choice(quotes).strip()
+            return quote.decode('utf-8')
+        else:
+            return "Database is empty"
